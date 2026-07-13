833ca4a7e9292fbaab21982be435544a40b7c97e
Nick Petersen <ncpete01@gmail.com>
2026-07-13 13:42:44 -0400

    Updated EKF Files for rbias offset

--- DiscreteStateEstimator.m
+++ DiscreteStateEstimator.m
@@ -1,8 +1,8 @@
 clear all
 syms Vy Vx r Psi Ax Rbias Abias Vypri Vxpri rpri Psipri Axpri Rbiaspri Abiaspri Mv B D C B E Iz delta a b Ts 
 
-AlphaF = delta - atan((-Vypri+a*(rpri+Rbiaspri))/Vxpri);
-AlphaR =       -atan((-Vypri-b*(rpri+Rbiaspri))/Vxpri);
+AlphaF = delta - atan((-Vypri+a*(-rpri+Rbiaspri))/Vxpri);
+AlphaR =       -atan((-Vypri-b*(-rpri+Rbiaspri))/Vxpri);
 
 
 Ff = D*sin(C*atan(B*AlphaF-E*(B*AlphaF-atan(B*AlphaF))));
